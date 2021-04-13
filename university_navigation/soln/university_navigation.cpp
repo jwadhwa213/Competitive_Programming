5c9bf67b3bb0548e550033e48a3042d2ede1fbc4
Jasmine Kaur Wadhwa <jasminewadhwa21@gmail.com>
2021-04-13 17:18:54 +0530

    Add files via upload

--- university_navigation/soln/university_navigation.cpp
+++ university_navigation/soln/university_navigation.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
+#include <map>
 using namespace std;
+
+
+typedef pair<int,int> PII;
+
+long long NumofPaths(int m,int n, map<PII,long long>& paths)
+{
+	
+	
+	if(paths.find(PII(m,n))==paths.end())
+	{
+		
+		if (m==1 || n==1)
+		{
+			
+			return 1;
+		}
+		
+			
+		paths[PII(m,n)]=(NumofPaths(m-1,n,paths)+ NumofPaths(m,n-1,paths))%4201337;
+		
+		return paths[PII(m,n)];
+			
+	}
+	else{
+		
+		return paths[PII(m,n)];
+	}
+
+}
 
 int main() {
     // Good luck on your final morning problem! 
     // Great job getting through the semester!
+
+    int m,n;
+    
+
+    
+    map< PII , long long> paths;
+
+  
+
+    cin>>m>>n;
+
+    cout<<NumofPaths(m+1,n+1,paths)<<endl;;
+    
+
 
     return 0;
 }
