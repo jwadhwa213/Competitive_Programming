c8418f154574f1f40d7e38d2571aca43f3827faa
jwadhwa213 <jwadhwa@ualberta.ca>
2021-04-13 00:50:46 -0600

    Finding number of paths

---
+++ university_navigation/soln/university_navigation.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Good luck on your final morning problem! 
+    // Great job getting through the semester!
+
+    return 0;
+}
---
+++ university_navigation/soln/university_navigation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+g++ university_navigation.cpp -o university_navigation -Wall -std=c++11 && ./university_navigation
+rm -f ./university_navigation
